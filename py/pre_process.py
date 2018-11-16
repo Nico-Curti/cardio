053945f4d99a6c2d3aebf0acccae75d1f55cb486
Lyro96 <lorenzo.dallolio@studio.unibo.it>
2018-11-16 16:07:53 +0100

    changed fs calculation because some files has Time which starts from 10 seconds or more, added file name-code to pre-process plot (when view=True)

--- py/pre_process.py
+++ py/pre_process.py
@@ -8,23 +8,26 @@ import numpy as np
 import pandas as pd
 from scipy import signal#, interpolate
 
+
 m_avg = lambda t, x, w : (np.asarray([t[i] for i in range(w, len(x) - w)]),
                           np.convolve(x, np.ones((2*w + 1, )) / (2*w + 1),
                                       mode = 'valid'))
 
-def process_pipe(data, view = False, output = ""):
-  fs = len(data.Time)/max(data.Time)
-  # moving average
-  w_size = int(fs * .5)
-  mt, ms = m_avg(data.Time, data.R, w_size)
 
+def process_pipe(data, view=False, output="", name=""):  
+  fs = len(data.Time)/(max(data.Time)-min(data.Time))  # sample rate
+  
+  # moving average
+  w_size = int(fs*.5)  # width of moving window
+  mt, ms = m_avg(data.Time, data.R, w_size)  # computation of moving average 
+  
   # remove global modulation
   sign = (data.R.iloc[w_size : -w_size] - ms).values
 
   # compute signal envelope
   analytical_signal = np.abs(signal.hilbert(sign))
 
-  fs = len(sign) / max(mt)
+  fs = len(sign)/(max(mt)-min(mt))
   w_size = int(fs)
   # moving averate of envelope
   mt_new, mov_avg = m_avg(mt, analytical_signal, w_size)
@@ -32,20 +35,15 @@ def process_pipe(data, view = False, output = ""):
   # remove envelope
   signal_pure = sign[w_size : -w_size] / mov_avg
 
-  # spline interpolation of data
-#  t_new = np.arange(mt[0], mt[-1], 1./(len(mt) * 10)) # increase of f sampling of 10
-#  tck   = interpolate.splrep(mt, signal_pure, s=0)
-#  signal_upsample = interpolate.splev(t_new, tck, der=0)
-
   if view:
-
+    
     import matplotlib.pylab as plt
 
     fig, (ax1, ax2, ax3) = plt.subplots(nrows = 3, ncols = 1, figsize = (10, 8), sharex=True)
     ax1.plot(data.Time, data.R, "b-", label="Original")
     ax1.legend(loc='best')
-    ax1.set_title("Raw", fontsize=14)#, fontweight="bold")
-
+    ax1.set_title("File "+str(name)+" Raw", fontsize=14)#, fontweight="bold")
+    
     ax2.plot(mt, sign, 'r-', label="Pure signal")
     ax2.plot(mt_new, mov_avg, 'b-', label='Modulation', alpha=.5)
     ax2.legend(loc='best')
@@ -57,6 +55,7 @@ def process_pipe(data, view = False, output = ""):
 
     ax3.set_xlabel("Time (sec)", fontsize=14) # common axis label
     ax3.legend(loc='best')
+    
     fig.tight_layout()
     plt.savefig(output, bbox_inches='tight')
 
@@ -83,5 +82,3 @@ if __name__ == '__main__':
 
   data = pd.read_csv(filename, sep=",")
   process_pipe(data, view, output)
-
-
