e455ab54fe0958c938b8006989cab62cb6ee3ebf
Valentino Recaldini <recaldini.valentino@gmail.com>
2019-05-17 22:22:55 +0200

    functions for the extraction of the features a, b, c, d, e from SDPPG signals in PPG processing and a few tests to verify their reliability

---
+++ py/sdppg_features.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from scipy.interpolate import interp1d
+
+def find_first_index_for_maximum_in_zero_crossing(sdppg, zero_crossing):
+    """
+    Find the first index in the zero_crossing array that corresponds to the
+    leftmost end of a zero crossing interval for a local maximum.
+
+    Parameters
+    ----
+    sdppg: 1D array-life of float; the sdppg signal
+    zero_crossing: 1D array-like of float; the zero crossing found for sdppg through its second derivative
+
+    Returns
+    ----
+    start: int; leftomst end of the interval containing a local maximum
+    """
+    if len(zero_crossing) == 0:
+        return 0
+    start = 0
+    # compute max and min values
+    max_value = np.max(sdppg[zero_crossing[start]:zero_crossing[start+1]])
+    min_value = np.min(sdppg[zero_crossing[start]:zero_crossing[start+1]])
+    # get the relative amplitude of the peak/trough
+    max_value = np.max([max_value - sdppg[zero_crossing[start]],
+                        max_value - sdppg[zero_crossing[start+1]]])
+    min_value = np.max([sdppg[zero_crossing[start]] - min_value,
+                        sdppg[zero_crossing[start+1]] - min_value])
+    if min_value > max_value:
+        start += 1
+    return start
+
+
+def find_next_a(sdppg, zero_crossing, start):
+    """
+    Find the next a peak in the SDPPG signal.
+
+    Parameters
+    ----
+    sdppg: 1D array-like of float; the (normalised) sdppg signal
+    zero_crossing: 1D array-like of float; the zero crossing found from the fourth derivative of the PPG
+    start: int, starting position from which the zero_crossing array will be read
+
+    Returns
+    ----
+    a: float; the feature
+    k: int; the first position in the zero-crossing array after a
+    
+    Notes: if there is no next a, a is set to np.nan and k ito len(zero_crossing)
+    For further information:
+        https://ieeexplore.ieee.org/document/5412099
+    """
+    k = start
+
+    def compute_max(x, z, n):  # TODO maybe out?
+        return np.max(x[z[n]:z[n+1]])
+    # TODO check if there are enough elements in z before doing this
+    z_len = len(zero_crossing)
+    if k + 3 >= z_len:
+        return np.nan, z_len
+    max1 = compute_max(sdppg, zero_crossing, k)
+    max2 = compute_max(sdppg, zero_crossing, k+2)   # k+1 refers to the next min
+    while max2 > max1:
+        k += 2
+        max1 = max2
+        if k + 3 >= z_len:
+            return np.nan, z_len
+        max2 = compute_max(sdppg, zero_crossing, k+2)
+    # max1 is a
+    k += 1
+    return max1, k
+
+
+def features_from_sdppg(t, signal, l=100, flip=True):
+    """
+    Find the a, b, c, d, e parameters for a PPG signal through the normalised
+    spline of its second derivative.
+
+    Parameters
+    ----
+    t: 1D array-like of float; the time points at which the signal has been measured
+    signal: 1D array-like of float; the measured PPG signal
+    l: int; factor used to compute the new number of points: the length of the splined signal will be len(t)*l
+    flip: bool; should the signal be flipped?
+    NOTES: t and signal must have the same length; t must be monotonic and positive
+
+    Returns
+    ----
+    a dictionary containing a, b, c, d, e: the features extracted from the SDPPG.
+
+    For further information, please read:
+        https://www.hindawi.com/journals/tswj/2013/169035/abs/
+        https://ieeexplore.ieee.org/document/5412099
+    """
+    sdppg = np.gradient(np.gradient(signal, t), t)
+    fdppg = np.gradient(np.gradient(sdppg, t), t)
+    spline_sdppg = interp1d(t, sdppg, kind='cubic')
+    spline_fdppg = interp1d(t, fdppg, kind='cubic')
+    newt = np.linspace(t[0], t[-1], len(t)*l)
+    spline_sdppg = spline_sdppg(newt)  # TODO change name?
+    spline_fdppg = spline_fdppg(newt)
+    # normalisation
+    spline_sdppg /= np.max(spline_sdppg)
+    spline_fdppg /= np.max(spline_fdppg)
+    # flip
+    if flip:
+        spline_sdppg = np.flip(spline_sdppg)
+        spline_fdppg = np.flip(spline_fdppg)
+    # find the zero crossings
+    intermediate_step = spline_fdppg[0:-1]*spline_fdppg[1:]
+    zero_crossing = np.where(intermediate_step < 0.)[0]
+    # find starting position
+    i = find_first_index_for_maximum_in_zero_crossing(
+            spline_sdppg, zero_crossing)
+    a = []
+    b = []
+    c = []
+    d = []
+    e = []
+    z_len = len(zero_crossing)
+    while i < z_len:
+        next_a, i = find_next_a(spline_sdppg, zero_crossing, i)
+        if i + 4 >= z_len or next_a == np.nan:
+            break
+
+        next_b = np.min(spline_sdppg[zero_crossing[i]:zero_crossing[i+1]])
+        next_c = np.max(spline_sdppg[zero_crossing[i+1]:zero_crossing[i+2]])
+        next_d = np.min(spline_sdppg[zero_crossing[i+2]:zero_crossing[i+3]])
+        next_e = np.max(spline_sdppg[zero_crossing[i+3]:zero_crossing[i+4]])
+        # No need to check c: it's the next maximum after a; always < a
+        if next_e >= next_a:
+            i += 3  # Find the next 'a' after the bad region due to e
+        else:
+            a.append(next_a)
+            b.append(next_b)
+            c.append(next_c)
+            d.append(next_d)
+            e.append(next_e)
+            i += 5  # i+4 -> i+5 should have a minimum inside
+
+    return {'a': a, 'b': b, 'c': c, 'd': d, 'e': e}  # return a dictionary
---
+++ test_py/test_sdppg.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import sys
+sys.path.append("../py")
+import sdppg_features
+import pytest
+
+
+def test_find_first_index_for_maximum_in_zero_crossing():
+    # test empty zero crossing
+    t = np.linspace(0, 2*np.pi, 600)
+    start = sdppg_features.find_first_index_for_maximum_in_zero_crossing(
+            t, [])
+    assert start == 0
+    s = np.sin(t)
+    # 2PI is the period. We expect start to be 0
+    z = [0, int(len(s)/2), len(s)-1]
+    start = sdppg_features.find_first_index_for_maximum_in_zero_crossing(
+            s, z)
+    assert start == 0
+
+
+def test_find_next_a():
+    x = np.linspace(3, 0, 600)
+    t = np.linspace(0, 8*np.pi, 600) 
+    s = np.sin(t)*x
+    z = [0] + [int(len(s)/i) for i in np.arange(8, 0, -1)]
+    a, k = sdppg_features.find_next_a(s, z, 0)
+    assert a == np.max(s)
+    assert k == 1
+
+
+def test_features_from_sdppg():
+    x = np.linspace(3, 0, 600)
+    t = np.linspace(0, 8*np.pi, 600) 
+    s = np.sin(t)*x
+    f = sdppg_features.features_from_sdppg(t, s)
+    assert len(f) == 5
+    for _, __ in zip(list(f)[:], list(f)[1:]):
+        assert len(f[_]) == len(f[__])
