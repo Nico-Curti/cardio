5511e34dc045d4de55516db3f4756d6d13c12d7c
Lorenzo Dall'Olio <lyro.anatnas@gmail.com>
2019-05-19 20:17:20 +0200

    added spline option to sdppg_features, removed useless control, corrected typo and docstrings

--- py/sdppg_features.py
+++ py/sdppg_features.py
@@ -3,6 +3,7 @@
 import numpy as np
 from scipy.interpolate import interp1d
 
+
 def find_first_index_for_maximum_in_zero_crossing(sdppg, zero_crossing):
     """
     Find the first index in the zero_crossing array that corresponds to the
@@ -10,12 +11,12 @@ def find_first_index_for_maximum_in_zero_crossing(sdppg, zero_crossing):
 
     Parameters
     ----
-    sdppg: 1D array-life of float; the sdppg signal
+    sdppg: 1D array-like of float; the sdppg signal
     zero_crossing: 1D array-like of float; the zero crossing found for sdppg through its second derivative
 
     Returns
     ----
-    start: int; leftomst end of the interval containing a local maximum
+    start: int; leftmost end of the interval containing a local maximum
     """
     if len(zero_crossing) == 0:
         return 0
@@ -47,8 +48,8 @@ def find_next_a(sdppg, zero_crossing, start):
     ----
     a: float; the feature
     k: int; the first position in the zero-crossing array after a
-    
-    Notes: if there is no next a, a is set to np.nan and k ito len(zero_crossing)
+
+    Notes: if there is no next a, a is set to np.nan and k to len(zero_crossing)
     For further information:
         https://ieeexplore.ieee.org/document/5412099
     """
@@ -61,19 +62,19 @@ def find_next_a(sdppg, zero_crossing, start):
     if k + 3 >= z_len:
         return np.nan, z_len
     max1 = compute_max(sdppg, zero_crossing, k)
-    max2 = compute_max(sdppg, zero_crossing, k+2)   # k+1 refers to the next min
+    max2 = compute_max(sdppg, zero_crossing, k+2)  # k+1 refers to the next min
     while max2 > max1:
         k += 2
         max1 = max2
         if k + 3 >= z_len:
             return np.nan, z_len
         max2 = compute_max(sdppg, zero_crossing, k+2)
-    # max1 is a
+    # now max1 is a
     k += 1
     return max1, k
 
 
-def features_from_sdppg(t, signal, l=100, flip=True):
+def features_from_sdppg(t, signal, flip=True, spline=True, f=100):
     """
     Find the a, b, c, d, e parameters for a PPG signal through the normalised
     spline of its second derivative.
@@ -82,13 +83,15 @@ def features_from_sdppg(t, signal, l=100, flip=True):
     ----
     t: 1D array-like of float; the time points at which the signal has been measured
     signal: 1D array-like of float; the measured PPG signal
-    l: int; factor used to compute the new number of points: the length of the splined signal will be len(t)*l
-    flip: bool; should the signal be flipped?
+    flip: bool (default=True); should the signal be flipped?
+    spline: bool (default=True); use the cubic spline interpolation of signal instead of signal
+    f: int (default=100); factor used to compute the new number of points if spline==True : the length of the splined signal will be len(t)*l
+
     NOTES: t and signal must have the same length; t must be monotonic and positive
 
     Returns
     ----
-    a dictionary containing a, b, c, d, e: the features extracted from the SDPPG.
+    dictionary containing 5 lists: a, b, c, d, e: each of them is a list of amplitudes for a, b, c, d, e peaks found in SDPPG.
 
     For further information, please read:
         https://www.hindawi.com/journals/tswj/2013/169035/abs/
@@ -96,24 +99,28 @@ def features_from_sdppg(t, signal, l=100, flip=True):
     """
     sdppg = np.gradient(np.gradient(signal, t), t)
     fdppg = np.gradient(np.gradient(sdppg, t), t)
-    spline_sdppg = interp1d(t, sdppg, kind='cubic')
-    spline_fdppg = interp1d(t, fdppg, kind='cubic')
-    newt = np.linspace(t[0], t[-1], len(t)*l)
-    spline_sdppg = spline_sdppg(newt)  # TODO change name?
-    spline_fdppg = spline_fdppg(newt)
+    # spline
+    if spline:
+        spline_sdppg = interp1d(t, sdppg, kind='cubic')
+        spline_fdppg = interp1d(t, fdppg, kind='cubic')
+        newt = np.linspace(t[0], t[-1], len(t)*f)
+        spline_sdppg = spline_sdppg(newt)
+        spline_fdppg = spline_fdppg(newt)
+        sdppg = spline_sdppg
+        fdppg = spline_fdppg
     # normalisation
-    spline_sdppg /= np.max(spline_sdppg)
-    spline_fdppg /= np.max(spline_fdppg)
+    sdppg /= np.max(np.abs(sdppg))
+    fdppg /= np.max(np.abs(fdppg))
     # flip
     if flip:
-        spline_sdppg = np.flip(spline_sdppg)
-        spline_fdppg = np.flip(spline_fdppg)
+        sdppg = np.flip(sdppg)
+        fdppg = np.flip(fdppg)
     # find the zero crossings
-    intermediate_step = spline_fdppg[0:-1]*spline_fdppg[1:]
+    intermediate_step = fdppg[0:-1]*fdppg[1:]
     zero_crossing = np.where(intermediate_step < 0.)[0]
     # find starting position
     i = find_first_index_for_maximum_in_zero_crossing(
-            spline_sdppg, zero_crossing)
+            sdppg, zero_crossing)
     a = []
     b = []
     c = []
@@ -121,23 +128,23 @@ def features_from_sdppg(t, signal, l=100, flip=True):
     e = []
     z_len = len(zero_crossing)
     while i < z_len:
-        next_a, i = find_next_a(spline_sdppg, zero_crossing, i)
-        if i + 4 >= z_len or next_a == np.nan:
+        next_a, i = find_next_a(sdppg, zero_crossing, i)
+        if i + 4 >= z_len:
             break
 
-        next_b = np.min(spline_sdppg[zero_crossing[i]:zero_crossing[i+1]])
-        next_c = np.max(spline_sdppg[zero_crossing[i+1]:zero_crossing[i+2]])
-        next_d = np.min(spline_sdppg[zero_crossing[i+2]:zero_crossing[i+3]])
-        next_e = np.max(spline_sdppg[zero_crossing[i+3]:zero_crossing[i+4]])
+        next_b = np.min(sdppg[zero_crossing[i]:zero_crossing[i+1]])
+        next_c = np.max(sdppg[zero_crossing[i+1]:zero_crossing[i+2]])
+        next_d = np.min(sdppg[zero_crossing[i+2]:zero_crossing[i+3]])
+        next_e = np.max(sdppg[zero_crossing[i+3]:zero_crossing[i+4]])
         # No need to check c: it's the next maximum after a; always < a
         if next_e >= next_a:
-            i += 3  # Find the next 'a' after the bad region due to e
+            i += 3  # Find the next 'a' after the bad region due to false e
         else:
             a.append(next_a)
             b.append(next_b)
             c.append(next_c)
             d.append(next_d)
             e.append(next_e)
-            i += 5  # i+4 -> i+5 should have a minimum inside
+            i += 5  # sdppg[zero_crossing[i+4]: zero_crossing[i+5]] should be convex
 
     return {'a': a, 'b': b, 'c': c, 'd': d, 'e': e}  # return a dictionary
