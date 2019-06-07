5f7742ddfc29f5539c053220e1abf34143d3ee50
Valentino Recaldini <recaldini.valentino@gmail.com>
2019-06-07 16:55:31 +0200

    Created .py file to load and clean a dataframe for further processing; created the docstrings and the tests accordingly.

---
+++ py/clean_db.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+import json
+import numpy as np
+import pandas as pd
+import argparse
+
+
+def db_to_dataframe(filename):
+  """
+  Load the json file obtained through create_db into a pandas dataframe
+
+  Parameters
+  ----
+  filename: string; the full path to the json file, complete with extension
+
+  Return
+  ----
+  d: DataFrame; the dataframe containing the elements stored in the file
+  """
+  d = json.load(open(filename))
+  d = pd.DataFrame(d).T
+  d = d.set_index(np.arange(len(d)))
+  return d
+
+
+def clean_db(db, drop_columns=['rhythm'],
+             drop_na_in=['weight', 'tpr', 'madRR', 'medianRR', 'opt_delay',
+                      'afib', 'age', 'sex'],
+              drop_zeroes_in=['weight', 'age'],
+              quality_threshold=None,
+              reset_index=True
+              ):
+  """
+  Clean the 'cardio' dataframe by removing the 'bad' elements
+
+  Parameters
+  ----
+  db: pandas DataFrame; the dataframe contaning the cardio data extracted through create_db
+  drop_columns: list of strings; (optional; default: ['rhythm']); the column labels that must be dropped from the dataframe. If None, it is skipped
+  drop_na_in: list of strings; (optional; default: 'weight', 'tpr', 'madRR', 'medianRR', 'opt_delay', 'afib', 'age', 'sex']); the subset of labels for which we must drop the row with NAN values. If None, it is skipped
+  drop_zeroes_in: list of strings; (optional; default: ['weight', 'age']); the subset of labels for which we must drop the zero values because they bear no meaning. If None, it is skipped.
+  quality_threshold: float; (optional; default: None); the quality threshold above which we drop the elements in the dataframe. BEWARE: if it is not None, the label 'quality' must be present!
+  reset_index: bool; (default: True); if True reset the index so as to be able to call the elements by row
+
+  Return
+  ---
+  newdb: pandas DataFrame; the cleaned dataframe
+  """
+  newdb = db
+  if quality_threshold is not None:
+    newdb = newdb[newdb['quality'] < quality_threshold]
+  if drop_columns is not None:
+    dcn = list(set(drop_columns) & set(newdb.columns))
+    if len(dcn) != 0:
+      newdb = newdb.drop(columns=dcn)
+  if drop_na_in is not None:
+    dnn = list(set(drop_na_in) & set(newdb.columns))
+    if len(dnn) != 0:
+      newdb = newdb.dropna(subset=dnn)
+  if drop_zeroes_in is not None:
+    dzn = list(set(drop_zeroes_in) & set(newdb.columns))
+    if len(dzn) != 0:
+      for _ in dzn:
+        newdb = newdb[newdb[_].astype(float) > 0]
+  if reset_index == True:
+    newdb = newdb.set_index(np.arange(len(newdb)))
+  return newdb
+
+
+if __name__ == 'main':
+  descr = "Load and clean database from json file."
+  parser = argparse.ArgumentParser(description=descr)
+  parser.add_argument("-f", required=False,  dest="filename", action="store", help="Database filename", default='cardio.json')
+  # get filename
+  filename = parser.filename
+  df = db_to_dataframe(filename)
+  df = clean_db(df)
+
+
---
+++ test_py/test_clean_db.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import json
+import pandas as pd
+import sys
+import os
+sys.path.append("../py")
+import clean_db
+
+def test_db_to_dataframe():
+  f = {}
+  filename = "f.json"
+  with open(filename, 'w') as file:
+    json.dump(f, file)
+  d = clean_db.db_to_dataframe(filename)
+  assert isinstance(d, pd.DataFrame)
+  assert d.empty
+  os.remove(filename)
+
+
+def test_clean_db_optionalNone():
+  d = pd.DataFrame()
+  N = 100
+  d['quality'] = np.linspace(0, 1E-3, 5*N)
+  d['rhythm'] = np.asarray([1, 2, 3, 4, np.nan]*N)
+  d['weight'] = np.asarray(np.asarray([0,]*N + list(np.arange(N))*4))
+  d['age'] = d['weight']
+  df = clean_db.clean_db(d, drop_columns=None, drop_na_in=None,
+                         drop_zeroes_in=None, quality_threshold=None)
+  assert len(df) == len(d)
+
+
+def test_clean_db_quality():
+  d = pd.DataFrame()
+  N = 100
+  d['quality'] = np.linspace(0, 1E-3, 5*N)
+  d['rhythm'] = np.asarray([1, 2, 3, 4, np.nan]*N)
+  d['weight'] = np.asarray(np.asarray([0,]*N + list(np.arange(N))*4))
+  d['age'] = d['weight']
+  df = clean_db.clean_db(d, drop_columns=None, drop_na_in=None,
+                         drop_zeroes_in=None, quality_threshold=1E-4)
+  assert max(df.quality) < 1E-4
+  assert len(df) < len(d)
+  assert len(df) == sum(np.where(d.quality < 1E-4, 1, 0))
+
+
+def test_clean_db_drop_columns():
+  d = pd.DataFrame()
+  N = 100
+  d['quality'] = np.linspace(0, 1E-3, 5*N)
+  d['rhythm'] = np.asarray([1, 2, 3, 4, np.nan]*N)
+  d['weight'] = np.asarray(np.asarray([0,]*N + list(np.arange(N))*4))
+  d['age'] = d['weight']
+  drop_columns_df=['rhythm', 'weight']
+  df = clean_db.clean_db(d, drop_columns=drop_columns_df,
+                          drop_na_in=None, drop_zeroes_in=None,
+                          quality_threshold=None)
+  df_col = list(set(df.columns) & set(drop_columns_df))
+  assert len(df_col) == 0
+  assert len(df) == len(d)
+
+
+def test_clean_db_drop_na():
+  d = pd.DataFrame()
+  N = 100
+  d['quality'] = np.linspace(0, 1E-3, 5*N)
+  d['rhythm'] = np.asarray([1, 2, 3, 4, np.nan]*N)
+  d['weight'] = np.asarray(np.asarray([0,]*N + list(np.arange(N))*4))
+  d['age'] = d['weight']
+  df = clean_db.clean_db(d, drop_columns=None,
+                          drop_na_in=['rhythm'], drop_zeroes_in=None,
+                          quality_threshold=None)
+  assert len(df) == 4*N
+  assert len(df.dropna(subset=('rhythm',))) == len(df)
+
+
+def test_clean_db_drop_zeroes():
+  d = pd.DataFrame()
+  N = 100
+  d['quality'] = np.linspace(0, 1E-3, 5*N)
+  d['rhythm'] = np.asarray([1, 2, 3, 4, np.nan]*N)
+  d['weight'] = np.asarray(np.asarray([0,]*N + list(np.arange(N))*4))
+  d['age'] = d['weight']
+  zeroes_labels = ['age', 'weight']
+  df = clean_db.clean_db(d, drop_columns=None,
+                          drop_na_in=None, drop_zeroes_in=zeroes_labels,
+                          quality_threshold=None)
+  assert len(df) == len(d) - 104  # 100 zeroes + 4 zeroes
+  for _ in zeroes_labels:
+    assert min(abs(df[_])) > 0
+
+
