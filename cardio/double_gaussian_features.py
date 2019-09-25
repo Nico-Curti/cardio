98957830b03bf62c63019cc1840bf0b926c86d9a
Lorenzo Dall'Olio <lyro.anatnas@gmail.com>
2019-09-25 13:58:14 +0200

    added package named cardio

---
+++ cardio/double_gaussian_features.py
@@ -0,0 +1,241 @@
+
+# -*- coding: utf-8 -*-
+
+# cardio stuff
+import create_db
+import pre_process as pr
+
+# standard libraries
+import numpy as np
+from scipy.optimize import curve_fit
+
+
+def find_x_of_minima(time, signal):
+  """
+  find index position of local minima whose amplitude is under a certain
+  moving threshold
+
+  Parameters
+  ----
+  time: numerical 1-D array-like; basically the x axis of the curve whose
+  minima will be found
+
+  signal: numerical 1-D array-like; basically the y axis of the curve whose
+  minima will be found
+
+  Return
+  ----
+  final_peaks: list; the list containing the index positions of signal minima
+
+  Example
+  ----
+  >>>import numpy as np
+  >>>
+  >>>x = np.linspace(0., 10*np.pi, 10000)
+  >>>y = np.sin(x)
+  >>>x_of_minima = find_x_of_minima(x, y)
+  >>>
+  >>>x_of_minima
+    Out:
+    array([1500, 3500, 5499, 7499])
+  >>>x[x_of_minima]
+    Out:
+    array([ 4.71286027, 10.99667395, 17.27734574, 23.56115943])
+  >>>y[x_of_minima]
+    Out:
+    array([-0.99999989, -0.9999994 , -0.999999  , -0.99999969])
+  """
+
+  sign = -1*np.asarray(signal)  # -1* is used to find minima instead of maxima
+
+  # using time to extrapolate sampling rate
+  srate = len(time)/(max(time)-min(time))
+  peaks = np.arange(len(sign))  # initializing peaks index
+
+  # different widths used for moving window, the unit is referred to 1 srate
+  for i in np.array([.5, 1., 1.5, 2., 3.]):
+    mt, mov_avg = pr.m_avg(time, sign, int(srate*i))
+
+    # use len_filler to make mov_avg the same size as sign
+    len_filler = np.zeros((len(sign)-len(mov_avg))//2) + np.mean(sign)
+    mov_avg = np.insert(mov_avg, 0, len_filler)
+    mov_avg = np.append(mov_avg, len_filler)
+
+    peaklist, sign_peak = create_db.detect_peaks(sign, mov_avg)
+
+    # keeping only peaks detected with all 5 different windows
+    peaks = np.intersect1d(peaks, peaklist)
+
+  # this check will be repeated after possible elimination of first and last peak
+  if(len(peaks) < 2):
+    raise ValueError(
+      "found {} minima BEFORE their validity check. We need at least 2 "
+      "to identify an isolated peak and perform fit. Maybe you are not using "
+      "a long enough portion of signal. If you want to analyze a single "
+      "peak make sure you have at least 1 detectable local minima before and "
+      "1 after it".format(len(peaks)))
+
+  # first element can't be a correct local extrema, it has no points before
+  if(peaks[0] == 0):
+    peaks = np.delete(peaks, 0)
+
+  # last element can't be a correct local extrema, it has no points after
+  if(peaks[-1] == len(sign)-1):
+    peaks = np.delete(peaks, -1)
+
+  # repeating check after
+  if(len(peaks) < 2):
+    raise ValueError(
+      "found {} minima AFTER their validity check, but we need at least 2 "
+      "to identify an isolated peak and perform fit. Please note that the "
+      "first/last point of the signal can not be a local minimum since "
+      "it does not have a point before/after. Maybe you are not using "
+      "a long enough portion of signal. If you want to analyze a single "
+      "peak make sure you have at least 1 detectable local minima before and "
+      "1 after it".format(len(peaks)))
+
+
+  # peak checking: rejecting lower peaks where RR distance is too small
+  final_peaks = []  # definitive peak positions container
+  last_peak = -1  # parameter to avoid undesired peaks still in final_peaks
+  for p in peaks:
+    if p <= last_peak:
+      continue
+
+    evaluated_peaks = [g for g in peaks if p <= g <= srate*.5+p]
+    last_peak = max(evaluated_peaks)
+    final_peaks.append(evaluated_peaks[np.argmin([sign[x] for x in evaluated_peaks])])
+
+  final_peaks = np.unique(final_peaks)  # to avoid repetitions
+
+  return final_peaks
+
+
+def features_from_dicrotic_notch(time, signal):
+  """
+  function used to extract features from differences between systolic and
+  diastolic peaks by performing a double gaussian fit.
+  The double gaussian fit is performed between two local minima,
+  so it needs a local minimum before the systolic peak and one after the
+  diastolic peak to work properly.
+
+  Parameters
+  ----
+  time: numerical 1-D array-like; basically the x axis of the curve to fit
+
+  signal: numerical 1-D array-like; basically the y axis of the curve to fit
+
+  Return
+  ----
+  parameter_list: list; list containing 6 parameters for each peak: maximum
+  height, mean and standard deviation of the first gaussian and same for the
+  second gaussian
+
+  total_beat_duration_list: list; list containing the range on x axis covered
+  by each single peak
+
+  total_beat_height_list: list; list containing the range on y axis covered
+  by each single peak
+
+  correct_x_of_maxima: list; list containing the x component of the maxima of
+  each single peak
+
+  Example
+  ----
+  >>>import numpy as np
+  >>>
+  >>>state = np.random.RandomState(seed = 42)
+  >>>
+  >>>mean1, var1 = 0. , .5
+  >>>mean2, var2 = 2. , 1.
+  >>>
+  >>>gauss1 = state.normal(mean1, var1, 1000000)
+  >>>gauss2 = state.normal(mean2, var2, 1000000)
+  >>>
+  >>># will be used also as x-axis
+  >>>binning = np.linspace(-4., 7., 100)
+  >>>
+  >>># summing the 2 gaussians, will be used as y-axis
+  >>>double_gaussian = np.histogram(gauss1, bins=binning)[0] + np.histogram(gauss2, bins=binning)[0]
+  >>>
+  >>># adding two spikes at either side of double_gaussian in order to obtain
+  >>># two local minima (one before and one after the signal) because they
+  >>># are needed for features_from_dictrotic_notch to identify the peak properly
+  >>>double_gaussian[1] = max(double_gaussian)
+  >>>double_gaussian[-1] = max(double_gaussian)
+  >>>
+  >>>
+  >>>features = features_from_dicrotic_notch(binning[:-1], double_gaussian)
+  >>>
+  >>>print("mean1:", mean1, "    predicted mean1:", features[0][0][1])
+  >>>print("var1:", var1, "    predicted var1:", features[0][0][2])
+  >>>print("mean2:", mean2, "    predicted mean1:", features[0][0][4])
+  >>>print("var2:", var2, "    predicted mean1:", features[0][0][5])
+    Out:
+    mean1: 0.0     predicted mean1: -0.055731720038204834
+    var1: 0.5     predicted var1: 0.5020164668217136
+    mean2: 2.0     predicted mean1: 1.9465910153756594
+    var2: 1.0     predicted mean1: 0.9993471020376136
+  """
+  x_of_minima = find_x_of_minima(time, signal)
+
+  parameter_list = []
+  total_beat_duration_list = []
+  total_beat_height_gap_list = []
+  correct_x_of_maxima = []
+
+  # function to initialize the fitting parameters (to avoid wrong convergence)
+  def pre_gaus(x, y):
+    x = np.asarray(x)
+    y = np.asarray(y)-min(y)
+
+    mean = sum(x * y) / sum(y)
+    sigma = np.sqrt(sum(y * (x - mean)**2) / sum(y))
+
+    return [max(y)+min(y), mean, sigma]
+
+  # function used in the fit
+  def gaus(x, a1, m1, s1, a2, m2, s2):
+    x = np.asarray(x)
+    return a1*np.exp(-(x-m1)**2/(2*s1**2))+a2*np.exp(-(x-m2)**2/(2*s2**2))
+
+  for i in range(len(x_of_minima)-1):
+    # separate the peak into its x and y component
+    X = np.asarray(time[x_of_minima[i]:x_of_minima[i+1]+1])
+    Y = np.asarray(signal[x_of_minima[i]:x_of_minima[i+1]+1])
+    Y = Y - min(Y)
+
+    lower_bounds = np.array([0, min(X), 0., -max(Y), min(X), 0.])
+    upper_bounds = np.array([max(Y), max(X), max(X)-min(X), max(Y), max(X),
+                             max(X)-min(X)])
+
+    # to initialize the 2 gaussians with different parameters we use pre_gaus
+    # on 2 different portions of the peak ([:split] and [-split:])
+    # we tried to vary split from 25% up to 75% of the peak lenght and it
+    # did not affected the output values of the fit for every test peaks used
+    # so we supposed it fixed to half of the lenght just for simplicity
+    split = int(len(X)*3/4)
+
+    try:
+
+      # NOTE the double "(" after np.concatenate is needed to work properly,
+      # since conatenate must work on a tuple in our case.
+      parameters, errors = curve_fit(gaus, X, Y,
+                                     p0=np.concatenate((pre_gaus(X[:split],
+                                                                Y[:split]),
+                                                       pre_gaus(X[-split:],
+                                                                Y[-split:]))),
+                                     bounds=(lower_bounds, upper_bounds))
+
+      parameter_list.append(parameters)
+      total_beat_duration_list.append(max(X)-min(X))
+      total_beat_height_gap_list.append(max(Y)-min(Y))
+      correct_x_of_maxima.append(X[np.argmax(Y)])
+
+    # skip problematic peaks
+    except RuntimeError:
+      continue
+    except TypeError:
+      continue
+
+  return parameter_list, total_beat_duration_list, total_beat_height_gap_list, correct_x_of_maxima
---
+++ cardio/sdppg_features.py
@@ -0,0 +1,291 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from scipy.interpolate import interp1d
+
+
+def find_first_index_for_maximum_in_zero_crossing(sdppg, zero_crossing):
+  """
+  Find the first index in the zero_crossing array that corresponds to the
+  leftmost end of a zero crossing interval for a local maximum.
+
+  Parameters
+  ----
+  sdppg: 1D array-like of float; the sdppg signal
+  zero_crossing: 1D array-like of float; the zero crossing found for sdppg through its second derivative
+
+  Returns
+  ----
+  start: int; index of the leftmost end of the interval containing a local maximum in the zero_crossing array
+
+  Example
+  ----
+  >>>import numpy as np
+  >>>x = np.linspace(-2.5*np.pi, 2.5*np.pi, 2000)
+  >>>f = np.sin(x)
+  >>>d2f = np.gradient(np.gradient(f, x), x)
+  >>>zero_crossing = np.where(d2f[0:-1]*d2f[1:] < 0.)[0]
+  >>>s = find_first_index_for_maximum_in_zero_crossing(f, zero_crossing)
+  >>>i1, i2 = zero_crossing[s], zero_crossing[s+1]
+  >>>np.max(f[i1:i2])  # expected 1: max(sin(x))
+    0.9999987650650776
+  >>>x[np.argmax(f[i1:i2])+i1]  # must be shifted by i1; expected -3*np.pi/2 (~ -4.712)
+    -4.710817398266836
+  """
+  if len(zero_crossing) == 0:
+    return 0
+  start = 0
+  # compute max and min values
+  max_value = np.max(sdppg[zero_crossing[start]:zero_crossing[start+1]])
+  min_value = np.min(sdppg[zero_crossing[start]:zero_crossing[start+1]])
+  # get the relative amplitude of the peak/trough
+  max_value = np.max([max_value - sdppg[zero_crossing[start]],
+                      max_value - sdppg[zero_crossing[start+1]]])
+  min_value = np.max([sdppg[zero_crossing[start]] - min_value,
+                      sdppg[zero_crossing[start+1]] - min_value])
+  if min_value > max_value:
+    start += 1
+  return start
+
+
+def find_next_a(sdppg, zero_crossing, start):
+  """
+  Find the next a peak in the SDPPG signal.
+
+  Parameters
+  ----
+  sdppg: 1D array-like of float; the (normalised) sdppg signal
+  zero_crossing: 1D array-like of float; the zero crossing found from the fourth derivative of the PPG
+  start: int, starting position from which the zero_crossing array will be read
+
+  Returns
+  ----
+  a: float; the feature
+  k: int; the first position in the zero-crossing array after a
+
+  Notes: if there is no next a, a is set to np.nan and k to len(zero_crossing)
+  For further information:
+      https://ieeexplore.ieee.org/document/5412099
+
+  Example
+  ----
+  >>># simulating a PPG signal as a sum of sinusoidal waves with frequences 1, 2, and 3 Hz
+  >>>t = np.linspace(-0.3, 1., 600)
+  >>>v = np.asarray([1., 2., 3.])
+  >>>s = 0
+  >>>for f in v:
+  ...  s += f**(-2)*np.sin(2*np.pi*f*t)
+  >>>sdppg = np.gradient(np.gradient(s, t), t)
+  >>>fdppg = np.gradient(np.gradient(sdppg, t), t)
+  >>>zero_crossing = np.where(fdppg[0:-1]*fdppg[1:] < 0.)[0]
+  >>>start = find_first_index_for_maximum_in_zero_crossing(sdppg, zero_crossing)
+  >>>find_next_a(sdppg, zero_crossing, start)  # (a, k)
+    Out:
+    (98.64553538733412, 2)
+  """
+  k = start
+
+  def compute_max(x, z, n):
+    return np.max(x[z[n]:z[n+1]])
+  z_len = len(zero_crossing)
+  if k + 3 >= z_len:
+    return np.nan, z_len
+  max1 = compute_max(sdppg, zero_crossing, k)
+  max2 = compute_max(sdppg, zero_crossing, k+2)  # k+1 refers to the next min
+  while max2 > max1:
+    k += 2
+    max1 = max2
+    if k + 3 >= z_len:
+      return np.nan, z_len
+    max2 = compute_max(sdppg, zero_crossing, k+2)
+  # now max1 is a
+  k += 1
+  return max1, k
+
+
+def sdppg_agi(a, b, c, d, e):
+  """
+  Compute the ageing index (AGI) from the features (a, b, c, d, e) extracted
+  from the SDPPG approach. The formula is (b-c-d-e)/a. The feature can be obtained through
+  features_from_sdppg.
+
+  Parameters
+  ----
+  a: float; array-like of float; feature 'a' extracted from the sdppg approach.
+  b: float; array-like of float; feature 'b' extracted from the sdppg approach.
+  c: float; array-like of float; feature 'c' extracted from the sdppg approach.
+  d: float; array-like of float; feature 'd' extracted from the sdppg approach.
+  e: float; array-like of float; feature 'e' extracted from the sdppg approach.
+
+  Returns
+  ----
+  AGI: numpy array of floats; the ageing index computed for each element.
+
+  For further reading:
+      https://www.hindawi.com/journals/tswj/2013/169035/abs/
+      https://ieeexplore.ieee.org/document/5412099
+
+  Example
+  ----
+  >>>a = [1, .8, .3]
+  >>>b = [-.9, -.7, -.2]
+  >>>c = [.1, .1, .1]
+  >>>d = [-.1, -.1, -.1]
+  >>>e = c
+  >>>sdppg_agi(a, b, c, d, e)
+  Out:
+    [-1., -1., -1.]
+  """
+  a_ = np.asarray(a, dtype=float)
+  b_ = np.asarray(b, dtype=float)
+  c_ = np.asarray(c, dtype=float)
+  d_ = np.asarray(d, dtype=float)
+  e_ = np.asarray(e, dtype=float)
+  AGI = (b_-c_-d_-e_)/a_
+  return AGI
+
+
+def features_from_sdppg(t, signal, normalise=True, flip=True,
+                        spline=True, f=100):
+  """
+  Find the a, b, c, d, e parameters (features, "waves") for a PPG signal
+  through its normalised second derivative.
+
+  Parameters
+  ----
+  t: 1D array-like of float; the time points at which the signal has been measured
+  signal: 1D array-like of float; the measured PPG signal
+  normalise: bool(default=True); if True, normalise SDPPG and its second derivative
+  flip: bool (default=True); should the signal be flipped?
+  spline: bool (default=True); use the cubic spline interpolation of signal instead of signal. BEWARE of splines: they can change the maxima and minima value and position!
+  f: int (default=100); factor used to compute the new number of points if spline==True : the length of the splined signal will be len(t)*f
+
+  Returns
+  ----
+  sdppg: the sdppg computed from the PPG signal provided. It is the result of the spline if spline=True
+  features: dictionary containing 10 numpy array: a, b, c, d, e (each of them is a np array of the respective "wave" fount in the SDPPG), the AGI index (AGI) computed for each element of the array, and the time differences between the "waves" a and b, b and c, c and d, and d and e (t_ab, t_bc, t_cd, t_de).
+
+  NOTES:
+    t and signal must have the same length; t must be monotonic and positive
+    a is the initial positive wave
+    b is early negative wave (we suppose b < 0)
+    c is the re-upsloping wave
+    d is the re-downloping wave
+    e is the diastolic positive wave
+
+  For further information, please read:
+      https://www.hindawi.com/journals/tswj/2013/169035/abs/
+      https://ieeexplore.ieee.org/document/5412099
+
+  Example
+  ----
+  >>># simulating a PPG signal as a sum of sinusoidal waves with frequences 1, 2, and 3 Hz
+  >>>t = np.linspace(-0.3, 1, 600)
+  >>>v = np.asarray([1., 2., 3.])
+  >>>s = 0
+  >>>for f in v:
+  ...  s += f**(-2)*np.sin(2*np.pi*f*t)
+  >>> t += .3  # avoiding negative t (optional)
+  >>>f, a = features_from_sdppg(t, s, normalise=False, flip=False, spline=False)
+  >>>a
+    Out:
+    {'a': array([98.64553539]),
+   'b': array([-98.64819372]),
+   'c': array([9.54438823]),
+   'd': array([-25.07132544]),
+   'e': array([25.06845097]),
+   'AGI': array([-1.0967522]),
+   't_ab': array([0.21268781]),
+   't_bc': array([0.18230384]),
+   't_cd': array([0.13238731]),
+   't_de': array([0.15843072])}
+  """
+  sdppg = np.gradient(np.gradient(signal, t), t)
+  fdppg = np.gradient(np.gradient(sdppg, t), t)
+  # spline
+  if spline:
+    spline_sdppg = interp1d(t, sdppg, kind='cubic')
+    spline_fdppg = interp1d(t, fdppg, kind='cubic')
+    newt = np.linspace(t[0], t[-1], len(t)*f)
+    spline_sdppg = spline_sdppg(newt)
+    spline_fdppg = spline_fdppg(newt)
+    sdppg = spline_sdppg
+    fdppg = spline_fdppg
+  else:
+    newt = np.asarray(t)
+  # normalisation
+  if normalise:
+    sdppg /= np.max(np.abs(sdppg))
+    fdppg /= np.max(np.abs(fdppg))
+  # flip
+  if flip:
+    sdppg = np.flip(sdppg)
+    fdppg = np.flip(fdppg)
+    newt = np.flip(newt)
+
+  # find the zero crossings
+  intermediate_step = fdppg[0:-1]*fdppg[1:]
+  zero_crossing = np.where(intermediate_step < 0.)[0]
+  # find starting position
+  i = find_first_index_for_maximum_in_zero_crossing(sdppg, zero_crossing)
+  a, b, c, d, e = [], [], [], [], []
+  t_a, t_b, t_c, t_d, t_e = [], [], [], [], []
+  z_len = len(zero_crossing)
+  while i < z_len:
+    next_a, i = find_next_a(sdppg, zero_crossing, i)
+    if i >= z_len:  # to be certain i is not out of bounds. See find_next_a
+      break
+    next_t_a = np.argmax(sdppg[zero_crossing[i-1]:zero_crossing[i]]) + zero_crossing[i-1]
+    next_b = 1.
+    while next_b > 0:
+      if i + 1 >= z_len:  # to be certain we have enough elements in the array
+        break
+      next_b = np.min(sdppg[zero_crossing[i]:zero_crossing[i+1]])
+      next_t_b = np.argmin(sdppg[zero_crossing[i]:zero_crossing[i+1]]) + zero_crossing[i]
+      if next_b > 0:
+        i += 2
+
+    if i + 4 >= z_len: # to be certain we have enough elements to find the other waves
+      break
+    # next_b = np.min(sdppg[zero_crossing[i]:zero_crossing[i+1]])
+    next_c = np.max(sdppg[zero_crossing[i+1]:zero_crossing[i+2]])
+    next_t_c = np.argmax(sdppg[zero_crossing[i+1]:zero_crossing[i+2]]) + zero_crossing[i+1]
+    next_d = np.min(sdppg[zero_crossing[i+2]:zero_crossing[i+3]])
+    next_t_d = np.argmin(sdppg[zero_crossing[i+2]:zero_crossing[i+3]]) + zero_crossing[i+2]
+    next_e = np.max(sdppg[zero_crossing[i+3]:zero_crossing[i+4]])
+    next_t_e = np.argmax(sdppg[zero_crossing[i+3]:zero_crossing[i+4]]) + zero_crossing[i+3]
+    # No need to check c: it's the next maximum after a; always < a
+    if next_e >= next_a:
+      i += 3  # Find the next 'a' after the bad region due to false e
+    else:
+      a.append(next_a)
+      b.append(next_b)
+      c.append(next_c)
+      d.append(next_d)
+      e.append(next_e)
+      t_a.append(next_t_a)
+      t_b.append(next_t_b)
+      t_c.append(next_t_c)
+      t_d.append(next_t_d)
+      t_e.append(next_t_e)
+      i += 5  # sdppg[zero_crossing[i+4]: zero_crossing[i+5]] should be a minimum
+
+  # convert to numpy array
+  a = np.asarray(a)
+  b = np.asarray(b)
+  c = np.asarray(c)
+  d = np.asarray(d)
+  e = np.asarray(e)
+  t_a = newt[t_a]
+  t_b = newt[t_b]
+  t_c = newt[t_c]
+  t_d = newt[t_d]
+  t_e = newt[t_e]
+  t_ab = np.abs(t_a-t_b)
+  t_bc = np.abs(t_b-t_c)
+  t_cd = np.abs(t_c-t_d)
+  t_de = np.abs(t_d-t_e)
+  agi = sdppg_agi(a, b, c, d, e)
+  features = {'a': a, 'b': b, 'c': c, 'd': d, 'e': e, 'AGI': agi,
+              't_ab': t_ab, 't_bc': t_bc, 't_cd': t_cd, 't_de': t_de}
+  return sdppg, features  # return sdppg and a dictionary
